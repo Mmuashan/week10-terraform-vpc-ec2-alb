4fc2a5e366c92e06acbad2631e2f7c9c4166f32a
Marius <tresormuashane@gmail.com>
2024-05-19 20:13:36 -0600

    Jenkins

--- variable.tf
+++ variable.tf
@@ -1,11 +1,20 @@
 variable "ENVIRONMENT" {
+  description = "The environment to deploy into"
+  type        = string
+  default     = "development"
    
 }
 
 variable "REGION" {
+  description = "The instance type"
+  type        = string
+  default     = "t2.micro"
   
 }
 
 variable "INSTANCE_TYPE" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"
   
 }
